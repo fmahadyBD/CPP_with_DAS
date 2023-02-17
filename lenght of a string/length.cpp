c6b1fc05bd850de9002e0685ebdf603d798a90cf
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-17 19:01:09 +0600

    Add files via upload

---
+++ lenght of a string/length.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string a;
+    cin>>a;
+    int b=a.length();
+    cout<<b<<endl;
+    int c=a.size();
+    cout<<c<<endl;
+   
+   return 0;
+}
