46873772fa0761e32828f705e2374929cf8abcd5
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-18 20:04:16 +0600

    Add files via upload

---
+++ Strings/clear.cpp
@@ -0,0 +1,14 @@
+/*----->>> Mahady Hasan Fahim
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string sr="Mahady Hasan Fahim";
+    sr.clear();
+    cout<<sr;
+   
+   return 0;
+}
