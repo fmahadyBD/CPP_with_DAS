792895cd09c3c77e0ebaa7cfbca3e9b02aae15df
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-18 20:06:32 +0600

    Add files via upload

---
+++ Strings/stringCompier.cpp
@@ -0,0 +1,15 @@
+/*----->>> Mahady Hasan Fahim
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ string s1="Mahady Hasan";  
+ string s2="Mahady Hasan";
+ if(s1.compare.s2==0){
+
+ }  
+   return 0;
+}
