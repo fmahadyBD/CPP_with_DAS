978512e6fa246e2a88436a3cb7e3bf669e98065d
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-18 20:07:11 +0600

    Add files via upload

---
+++ Strings/substrings.cpp
@@ -0,0 +1,14 @@
+/*----->>> Mahady Hasan Fahim
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ string s1="Mahady";
+ string s2=s1.substr(0,4);
+ cout<<s2;
+
+   return 0;
+}
