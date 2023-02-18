c339ad24d83ba174b141c5f9d774788e1c7b00a6
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-18 20:06:14 +0600

    Add files via upload

---
+++ Strings/sort.cpp
@@ -0,0 +1,15 @@
+/*----->>> Mahady Hasan Fahim
+*/
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+ string s="jjadgvmcziuhjscnjkgdh";
+  sort(s.begin(),s.end());
+  cout<<s;
+
+   return 0;
+}
