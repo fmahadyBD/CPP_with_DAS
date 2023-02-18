38e193be2868e78041d7e26c9ef9c72b9270e7ba
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-18 20:04:37 +0600

    Add files via upload

---
+++ Strings/empty.cpp
@@ -0,0 +1,15 @@
+/*----->>> Mahady Hasan Fahim
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ string s="kkk";
+ s.clear();
+ if(s.empty()){
+    cout<<"Emplty";
+ }  
+   return 0;
+}
