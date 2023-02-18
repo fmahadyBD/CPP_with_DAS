300918a13e0b428600e789972431b1feb7fac8c2
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-18 20:04:56 +0600

    Add files via upload

---
+++ Strings/erase.cpp
@@ -0,0 +1,14 @@
+/*----->>> Mahady Hasan Fahim
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ string s="abcdefghijklmnopqrstw";
+ s.erase(0,4);
+ //4 charecter will be removed
+ cout<<s;  
+   return 0;
+}
